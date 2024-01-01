5b24a44fe299366be3b150e338dfedf505f8b304
Justin Good <jgoodhcg@gmail.com>
2024-01-01 16:04:50 -0500

    Get data into xtdb locally

--- bb/src/tech/jgood/gleanmo/airtable_extract.clj
+++ bb/src/tech/jgood/gleanmo/airtable_extract.clj
@@ -1,30 +1,29 @@
 (ns tech.jgood.gleanmo.airtable-extract
   (:require [babashka.curl :as curl]
             [cheshire.core :as json]
-            [clojure.set :refer [rename-keys]]
+            [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
             [babashka.cli :as cli]
-            [clojure.string :refer [replace]]
-            ))
+            [clojure.string :refer [replace]]))
 
-(defn get-all-records [{:keys [api-key base-id table-id]}]
-  (let [url      (str "https://api.airtable.com/v0/" base-id "/" table-id)]
-    (loop [response (-> (curl/get
-                         url
-                         {:headers {"Authorization" (str "Bearer " api-key)}})
-                        (:body)
-                        (json/parse-string))
-           records []]
-      (let [offset (-> response (get "offset"))]
+(defn get-page [url api-key]
+  (-> (curl/get
+       url
+       {:headers {"Authorization" (str "Bearer " api-key)}})
+      (:body)
+      (json/parse-string)))
+
+(defn get-all-records [{:keys [api-key base-id table-id]} writer]
+  (let [url (str "https://api.airtable.com/v0/" base-id "/" table-id)]
+    (loop [response (get-page url api-key)]
+      (let [offset      (-> response (get "offset"))
+            new-records (-> response (get "records"))]
+        (doseq [record new-records]
+          (.write writer (pr-str record))
+          (.write writer "\n"))
         (if (nil? offset)
-          (concat records (-> response (get "records")))
-          (recur
-           (-> (curl/get
-                (str url "?offset=" offset)
-                {:headers {"Authorization" (str "Bearer " api-key)}})
-               (:body)
-               (json/parse-string))
-           (concat records (-> response (get "records")))))))))
+          nil
+          (recur (get-page (str url "?offset=" offset) api-key)))))))
 
 (defn timestamp []
   (-> (java.time.LocalDateTime/now)
@@ -38,10 +37,11 @@
             {:require [:api-key :base-id :table-id]
              :alias   {:k :api-key
                        :b :base-id
-                       :t :table-id}})]
-
-  (-> (get-all-records opts)
-    (#(with-out-str (pprint %)))
-    (->> (spit (str (timestamp) "_"
-                    (-> opts :table-id (replace "-" "_"))
-                    ".edn")))))
+                       :t :table-id}})
+      file-name (str (timestamp) "_"
+                     (-> opts :table-id (replace "-" "_"))
+                     ".edn")]
+  (with-open [writer (io/writer file-name)]
+    #_(.write writer "[\n")
+    (get-all-records opts writer)
+    #_(.write writer "]")))
--- src/tech/jgood/gleanmo/notebooks/20231231_airtable_ingest.clj
+++ src/tech/jgood/gleanmo/notebooks/20231231_airtable_ingest.clj
@@ -1,13 +1,118 @@
+;; # Ingesting Airtable Records
+;; I'm trying to accomplish two goals with this notebook.
+;; - Set data driven goals to improve my exercise habits
+;; - Draft transformation of data from airtable to something that works for xtdb/gleanmo
 (ns tech.jgood.gleanmo.notebooks.20231231-airtable-ingest
+  {:nextjournal.clerk/toc                   true
+   :nextjournal.clerk/error-on-missing-vars :off}
   (:require [nextjournal.clerk :as clerk]
             [semantic-csv.core :as sc]
             [clojure.java.io :as io]
+            [com.rpl.specter :as sp]
+            [clojure.edn :as edn]
+            [xtdb.api :as xt]
             [potpuri.core :as pot]
             [clojure-csv.core :as csv]
             [clojure.string :as str]
             [tick.core :as t]))
 
-;; Hello
-(def exercise-log-file "/notebook_data/2023-12-31__13_47_51_223729_exercise_log.edn")
+;; ## Read in data from files
+{::clerk/visibility {:code :fold :result :show}}
+(def exercise-file "notebook_data/2023-12-31__17_31_50_247708_exercises.edn")
+(def exercise-log-file "notebook_data/2023-12-31__14_07_28_274238_exercise_log.edn")
 
-(first (slurp exercise-log-file))
+;; ### Exercise-logs
+(with-open [rdr (io/reader exercise-log-file)]
+  (let [lines (line-seq rdr)]
+    (-> lines
+        (->> (map (fn [line]
+                    (-> line
+                        edn/read-string
+                        (->> (pot/map-keys keyword))
+                        (update :fields #(pot/map-keys keyword %))
+                        (update-in [:fields :timestamp]
+                                   #(if (or (nil? %)
+                                            (str/blank? %))
+                                      % (t/instant %)))))))
+        shuffle
+        (->> (take 2)))))
+
+;; ### Exercises
+(with-open [rdr (io/reader exercise-file)]
+  (let [lines (line-seq rdr)]
+    (-> lines
+        (->> (map (fn [line]
+                    (-> line
+                        edn/read-string
+                        (->> (pot/map-keys keyword))
+                        (update :fields #(pot/map-keys keyword %))))))
+        shuffle
+        (->> (take 2)))))
+
+;; ## Writing to XTDB
+;;
+;; ### XTDB Setup
+{::clerk/visibility {:code :fold :result :hide}}
+(defn start-xtdb! []
+  (letfn [(kv-store [dir]
+            {:kv-store {:xtdb/module 'xtdb.rocksdb/->kv-store
+                        :db-dir (io/file dir)
+                        :sync? true}})]
+    (xt/start-node
+     {:xtdb/tx-log (kv-store "notebook_data/xtdb/tx-log")
+      :xtdb/document-store (kv-store "notebook_data/xtdb/doc-store")
+      :xtdb/index-store (kv-store "notebook_data/xtdb/index-store")})))
+(defonce xtdb-node (start-xtdb!))
+(defn stop-xtdb! []
+  (.close xtdb-node))
+{::clerk/visibility {:code :fold :result :show}}
+
+;; ### Exercises
+(defn xform-exercise [{:keys [id createdTime fields]}]
+  (let [{:keys [name
+                notes
+                distance-unit
+                weight-unit
+                exercise-log
+                source
+                log-count
+                latest-done]} fields
+        new-uuid              (java.util.UUID/randomUUID)]
+    (->>
+     {:xt/id                  new-uuid
+      :xt/valid-time          (t/instant createdTime)
+      :gleanmo/type           :exercise
+      :exercise/label         name
+      :exercise/notes         notes
+      :exercise/source        source
+      :airtable/ported        true
+      :airtable/created-time  createdTime
+      :airtable/distance-unit distance-unit
+      :airtable/id            id
+      :airtable/exercise-log  exercise-log
+      :airtable/weight-unit   weight-unit
+      :airtable/log-count     log-count
+      :airtable/latest-done   latest-done}
+
+     ;; effectively dissoc's anything with a nil value
+     (sp/setval [sp/MAP-VALS nil?] sp/NONE))))
+
+(with-open [rdr (io/reader exercise-file)]
+  (let [lines (line-seq rdr)]
+    (doall
+     (-> lines
+         (->> (map (fn [line]
+                     (let [item
+                           (-> line
+                               edn/read-string
+                               (->> (pot/map-keys keyword))
+                               (update :fields #(pot/map-keys keyword %))
+                               xform-exercise)]
+                       (xt/submit-tx xtdb-node [[::xt/put item]])))))))))
+
+{::clerk/visibility {:code :show :result :show}}
+(xt/q (xt/db xtdb-node) `{:find [e]
+                          :where [[e :gleanmo/type :exercise]]})
+;; ### Sessions
+;; ### Logs
+;; ### Sets
