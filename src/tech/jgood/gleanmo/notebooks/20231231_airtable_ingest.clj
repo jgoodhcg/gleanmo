262ed187941aab9f37e6acf5e5d0a70c1fec58ea
Justin Good <jgoodhcg@gmail.com>
2024-01-02 17:18:52 -0500

    Start transforming exercise-logs

--- src/tech/jgood/gleanmo/notebooks/20231231_airtable_ingest.clj
+++ src/tech/jgood/gleanmo/notebooks/20231231_airtable_ingest.clj
@@ -67,15 +67,16 @@
 (defonce xtdb-node (start-xtdb!))
 (defn stop-xtdb! []
   (.close xtdb-node))
-{::clerk/visibility {:code :fold :result :show}}
 
 ;; ### Deterministic UUIDs
 ;; I want all of the ported items to have a consistent xt/id type -- UUID.
 ;; However, I will be iterating on processing and might not delete the entire xtdb each run.
 ;; I want to be able to run the porting __deterministically__ and not duplicate information.
 ;; So the UUID id needs to be deterministically derived from the airtable record.
+{::clerk/visibility {:code :fold :result :show}}
 (def namespace-uuid #uuid "ba5589b9-e2a2-47b9-9273-86206538c0e2")
 
+{::clerk/visibility {:code :fold :result :hide}}
 (defn generate-deterministic-uuid [seed]
   (uuid/v5 namespace-uuid seed))
 
@@ -125,13 +126,68 @@
          vec
          (->> (xt/submit-tx xtdb-node))))))
 
-{::clerk/visibility {:code :show :result :show}}
+{::clerk/visibility {:code :fold :result :show}}
 (xt/q (xt/db xtdb-node)
-      '{:find [?label ?id]
-        :where [[?e :gleanmo/type :exercise]
-                [?e :exercise/label ?label]
-                [?e :xt/id ?id]]})
+      '{:find [(pull ?e [*])]
+        :where [[?e :gleanmo/type :exercise]]})
+
+;; ### Logs & Sets
+;; Every airtable log is effectively a gleanmo log with one set.
+;; When tracking is done in gleanmo logs will have more than one set.
+(defn get-exercise-id [airtable-id]
+  (-> (xt/q (xt/db xtdb-node)
+            '{:find  [?id]
+              :where [[?e :gleanmo/type :exercise]
+                      [?e :airtable/id airtable-id]
+                      [?e :xt/id ?id]]
+              :in    [airtable-id]}
+            airtable-id)
+      first
+      first))
+
+(get-exercise-id "rec61KbEbx5uAVtAW")
+
+{::clerk/visibility {:code :show :result :show}}
+(defn xform-exercise-log [{:keys [id createdTime fields]}]
+  (let [{:keys [timestamp duration exercise]}
+        fields
+        exrcs-at-id (-> exercise first)]
+
+    (if (and (-> timestamp str/blank? not)
+             (-> exrcs-at-id str/blank? not)
+             (-> duration integer?))
+      (let [log-id      (generate-deterministic-uuid id)
+            set-id      (generate-deterministic-uuid (str id "-set"))
+            exercise-id (get-exercise-id exrcs-at-id)
+            beg         (t/instant timestamp)
+            end         (-> beg (t/>> (t/new-duration duration :seconds)))]
+        {:exercise-log {:xt/id                           log-id
+                        :gleanmo/type                    :exercise-log
+                        :exercise/id                     exercise-id
+                        :exercise-log.interval/beginning beg
+                        :exercise-log.interval/end       end
+                        :exercise-log/exercise-set-ids   #{set-id}}
+         :exercise-set {:xt/id set-id}})
+      nil)))
+
+(with-open [rdr (io/reader exercise-log-file)]
+  (let [lines (line-seq rdr)]
+    (doall
+     (-> lines
+         (->> (map (fn [line]
+                     (let [item
+                           (-> line
+                               edn/read-string
+                               (->> (pot/map-keys keyword))
+                               (update :fields #(pot/map-keys keyword %))
+                               xform-exercise-log)]
+                       (if (some? item)
+                         [::xt/put item]
+                         nil)))))
+         (->> (remove nil?))
+         shuffle
+         (->> (take 2))
+         #_#_vec
+           (->> (xt/submit-tx xtdb-node))))))
 
 ;; ### Sessions
-;; ### Logs
-;; ### Sets
