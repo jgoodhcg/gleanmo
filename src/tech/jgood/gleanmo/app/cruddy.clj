875b4077a8133db1fe11b71bf85fffd6453986ef
Justin Good <jgoodhcg@gmail.com>
2025-01-04 14:58:49 -0500

    Use crud ns

--- src/tech/jgood/gleanmo/app/cruddy.clj
+++ src/tech/jgood/gleanmo/app/cruddy.clj
@@ -1,21 +1,6 @@
 (ns tech.jgood.gleanmo.app.cruddy
-  (:require [tech.jgood.gleanmo.schema :refer [schema]]
-            [tech.jgood.gleanmo.crud :as crud]
-            [tech.jgood.gleanmo.app.shared :refer [side-bar]]
-            [potpuri.core :as pot]
-            [tech.jgood.gleanmo.ui :as ui]
-            [xtdb.api :as xt]
-            ))
+  (:require
+   [tech.jgood.gleanmo.crud :as crud]
+   [tech.jgood.gleanmo.schema :refer [schema]]))
 
-(defn root [{:keys [session biff/db]}]
-  (let [user-id              (:uid session)
-        {:user/keys [email]} (xt/entity db user-id)]
-    (ui/page
-     {}
-     [:div
-      (side-bar (pot/map-of email)
-                [:div.flex.flex-col.md:flex-row.justify-center
-                 [:h1.text-3xl.font-bold "Cruddy!"]])])))
-
-(def routes ["/crud" {}
-             ["" {:get root}]])
+(def routes (crud/gen-routes :cruddy schema))
--- src/tech/jgood/gleanmo/crud.clj
+++ src/tech/jgood/gleanmo/crud.clj
@@ -2,7 +2,23 @@
   (:require
    [clojure.pprint :as p]
    [com.biffweb :as biff]
-   [tech.jgood.gleanmo.middleware :as mid]
    [potpuri.core :as pot]
+   [tech.jgood.gleanmo.app.shared :refer [side-bar]]
+   [xtdb.api :as xt]
    [tech.jgood.gleanmo.ui :as ui]))
 
+(defn example-route [{:keys [session biff/db]}]
+  (let [user-id              (:uid session)
+        {:user/keys [email]} (xt/entity db user-id)]
+    (ui/page
+     {}
+     [:div
+      (side-bar (pot/map-of email)
+                [:div.flex.flex-col.md:flex-row.justify-center
+                 [:h1.text-3xl.font-bold "Cruddy!"]])])))
+
+(defn gen-routes [entity-key schema]
+  (let [schema          (entity-key schema)
+        entity-name-str (name entity-key)]
+    [(str "/crud/" entity-name-str) {}
+     ["" {:get example-route}]]))
