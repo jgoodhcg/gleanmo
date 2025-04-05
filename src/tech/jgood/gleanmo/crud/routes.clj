7ed62ee93ba9f61d485a6dee8b2ab929a200e86b
Justin Good <jgoodhcg@gmail.com>
2025-04-05 19:32:09 -0400

    Format routes namespace

--- src/tech/jgood/gleanmo/crud/routes.clj
+++ src/tech/jgood/gleanmo/crud/routes.clj
@@ -1,22 +1,22 @@
 (ns tech.jgood.gleanmo.crud.routes
-  (:require
-   [potpuri.core :as pot]
-   [tech.jgood.gleanmo.crud.forms :as forms]
-   [tech.jgood.gleanmo.crud.views :as views]))
+  (:require [potpuri.core :as pot]
+            [tech.jgood.gleanmo.crud.forms :as forms]
+            [tech.jgood.gleanmo.crud.views :as views]))
 
-(defn gen-routes [{:keys [entity-key schema plural-str entity-str]}]
+(defn gen-routes
+  [{:keys [entity-key schema plural-str entity-str]}]
   (let [schema (entity-key schema)
-        args   (pot/map-of entity-key
-                           schema
-                           plural-str
-                           entity-str)]
+        args   (pot/map-of entity-key schema plural-str entity-str)]
     ["/crud" {}
-     ;; new is preppended because the trie based router can't distinguish between
-     ;; /entity/new and /entity/:id
-     ;; this could be fixed with a linear based router but I think this is a fine REST convention to break from
-     [(str "/new/" entity-str)         {:get  (partial forms/new-form args)}]
-     [(str "/" entity-str)             {:get  (partial views/list-entities args)
-                                        :post (partial forms/create-entity! args)}]
-     [(str "/" entity-str "/:id/edit") {:get  (partial forms/edit-form args)}]
-     ;; [(str "/" entity-str "/:id")      {:post (partial forms/update-entity! args)}]
-     ]))
+     ;; new is preppended because the trie based router can't distinguish
+     ;; between /entity/new and /entity/:id this could be fixed with a
+     ;; linear based router but I think this is a fine REST convention to
+     ;; break from
+     [(str "/new/" entity-str) {:get (partial forms/new-form args)}]
+     [(str "/" entity-str)
+      {:get  (partial views/list-entities args),
+       :post (partial forms/create-entity! args)}]
+     [(str "/" entity-str "/:id/edit") {:get (partial forms/edit-form args)}]
+     ;; [(str "/" entity-str "/:id")      {:post (partial
+     ;; forms/update-entity! args)}]
+    ]))
